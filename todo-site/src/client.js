859413062620c621060b43e292398e3295ddb38e
Sebastian Gabriel Vinci <sgvinci@gmail.com>
2017-04-16 03:33:41 -0300

    docker swarm deployment

--- todo-site/src/client.js
+++ todo-site/src/client.js
@@ -6,7 +6,7 @@ import * as superagent from "superagent";
 export function get() {
 
     return new Promise((resolve, reject) => {
-        superagent.get("http://localhost:8100/todos")
+        superagent.get("/todos")
             .end((error, result) => {
                 error ? reject(error) : resolve(result.body);
             });
@@ -17,7 +17,7 @@ export function get() {
 export function add(text) {
 
     return new Promise((resolve, reject) => {
-        superagent.post("http://localhost:8100/todos")
+        superagent.post("/todos")
             .send({'text': text})
             .end((error, result) => {
                 error ? reject(error) : resolve(result.body);
@@ -29,7 +29,7 @@ export function add(text) {
 export function toggle(id) {
 
     return new Promise((resolve, reject) => {
-        superagent.patch("http://localhost:8100/todos/" + id)
+        superagent.patch("/todos/" + id)
             .end((error, result) => {
                 error ? reject(error) : resolve(result.body);
             });
